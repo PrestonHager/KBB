ba9ccfa573727f624d6387f82a787432f4af9dcc
PrestonHager <preston@hagerfamily.com>
2021-10-15 15:08:01 -0600

    Updated setup file.

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@
 import os
 
 def main():
-    os.system("git clone https://github.com/PrestonHager/KBB.git")
+    if os.path.exists(".git"):
+        print("Attempting to update git")
+        os.system("git pull")
+    else:
+        print("Cloning repository from github")
+        os.system("git clone https://github.com/PrestonHager/KBB.git")
+        print("Moving all files to main level")
+        os.system("mv KBB/ .")
+    os.system("python3 main.py")
 
 if __name__ == '__main__':
     main()
